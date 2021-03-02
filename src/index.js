28c7e95c06850732cb4c857e0a8cb01a3544c8f4
nero-15 <ti7805@gmail.com>
2021-03-02 22:33:56 +0900

    loading image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './toka2.jpg';
 
 
 function component() {
@@ -7,7 +8,13 @@ function component() {
 
 	//Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-	element.classList.add('hello');;
+	element.classList.add('hello');
+
+	// Add the image to our existing div.
+	const myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
 
 	return element;
 }
