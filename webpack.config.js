0e0996139ea5b4089feecdd40d31b5269ccdf9c2
nero-15 <ti7805@gmail.com>
2021-03-05 21:55:16 +0900

    Environment Variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	mode: 'development',
-	entry: {
-		index: './src/index.js',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Caching',
-		}),
-	],
-	output: {
-		filename: '[name].[contenthash].js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-		publicPath: '/',
-	},
-	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all',
+module.exports = (env) => {
+
+	console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+	console.log('Production: ', env.production); // true
+	return {
+		mode: 'development',
+		entry: {
+			index: './src/index.js',
+		},
+		devtool: 'inline-source-map',
+		devServer: {
+			contentBase: './dist',
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: 'Caching',
+			}),
+		],
+		output: {
+			filename: '[name].[contenthash].js',
+			path: path.resolve(__dirname, 'dist'),
+			clean: true,
+			publicPath: '/',
+		},
+		optimization: {
+			runtimeChunk: 'single',
+			splitChunks: {
+				cacheGroups: {
+					vendor: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendors',
+						chunks: 'all',
+					},
 				},
 			},
 		},
-	},
+	};
 };
