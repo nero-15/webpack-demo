a12c3ca69ffbe9e5cb0f4c5f4004f026e32ac2c9
nero-15 <ti7805@gmail.com>
2021-03-06 11:25:05 +0900

    Enabling HMR

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-async function getComponent() {
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
 	const element = document.createElement('div');
-	const { default: _ } = await import('lodash');
+	const btn = document.createElement('button');
+
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
+
 	return element;
 }
 
-getComponent().then((component) => {
-	document.body.appendChild(component);
-});
+document.body.appendChild(component());
+
+if (module.hot) {
+	module.hot.accept('./print.js', function() {
+		console.log('Accepting the updated printMe module!');
+		printMe();
+	})
+}
